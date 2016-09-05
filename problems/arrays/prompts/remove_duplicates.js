9622840193e10cc5a19033f1139b3b85aa0c4d72
Colin McDonald <mickydwow@gmail.com>
2016-09-05 14:48:02 -0700

    Remove duplicates

--- problems/arrays/prompts/remove_duplicates.js
+++ problems/arrays/prompts/remove_duplicates.js
@@ -3,6 +3,18 @@
 // returns an array without duplicates
 const removeDuplicates = (array) => {
   // your code here
+  var storage = {};
+  var newArr = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (!storage[array[i]]) {
+      newArr.push(array[i]);
+    } 
+    storage[array[i]] = true;
+  }
+
+  console.log(storage);
+  return newArr;
 };
 
 module.exports = { removeDuplicates };

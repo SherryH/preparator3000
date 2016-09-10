14330960f763dc83ad3333606b083b7061e4c238
Colin McDonald <mickydwow@gmail.com>
2016-09-10 13:07:10 -0700

    Some

--- problems/arrays/prompts/some.js
+++ problems/arrays/prompts/some.js
@@ -3,6 +3,13 @@
 // determine if some elements in the array pass a truth test
 const some = (array, truthTest) => {
   // your code here
+  var flag = false;
+  for (var i = 0; i < array.length; i++) {
+    if (truthTest(array[i])) {
+      flag = true;
+    }
+  }
+  return flag;
 };
 
 module.exports = { some };

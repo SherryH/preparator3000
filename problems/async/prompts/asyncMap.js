a41fb0d496fc432cfce244b30bbf4f71a4d527c2
SherryH <enjoy.wind7rain@gmail.com>
2017-04-05 06:18:48 +1200

    Completed asyncMap promise implementation

--- problems/async/prompts/asyncMap.js
+++ problems/async/prompts/asyncMap.js
@@ -17,41 +17,143 @@
  *
  * Example:
  *
+ * asyncMap([
+ *  cb => {
+ *    setTimeout(() => {
+ *      cb('one');
+ *    }, 200);
+ *  },
+ *  cb => {
+ *    setTimeout(() => {
+ *      cb('two');
+ *    }, 100);
+ *  }
+ * ],
+ *  results => {
+ *    // the results array will equal ['one','two'] even though
+ *    // the second function had a shorter timeout.
+ *    console.log(results); // ['one', 'two']
+ * });
  *
  *
  */
 
-
-const asyncMap = (tasks, callback) => {
-  var arr = [];
-  var i = 0;
-  function hitMe(result) {
-    arr.push(result);
-    i++
-    if (i === tasks.length) {
-      callback(arr)
-    } else {
-      tasks[i](hitMe)
-    }
+// Solve using Recursion
+var asyncMap = (tasks, callback) =>{
+  var task;
+  var answer = [];
+  console.log(tasks);
+  asyncMapRecur = () => {
+    if (tasks.length === 0) {
+      return callback(answer);
+    } 
+    task = tasks.shift();
+    task((val)=>{
+      answer.push(val);
+      asyncMapRecur();
+    });
   }
-  tasks[0](hitMe)
+  asyncMapRecur()
 };
 
+
+//Solve using foreach
+var asyncMap = (tasks, callback) => {
+  var answer = [];
+  var count = 0;
+  tasks.forEach((task, ind)=>{
+    task((val)=>{
+      answer[ind] = val;
+      count++;
+      //can't use answer.length in if comparison because answer.length doesn't reflect
+      // the actual num of items in the array -> it reflects the last index occupied
+      if(count === tasks.length) {
+        callback(answer);
+      }
+    });
+  });
+}
+
+const asyncMap = (tasks, callback) => {
+  var array = [];
+  var count = 0;
+  for (var i = 0; i < tasks.length; i++) {
+    (function(i){
+      tasks[i]((val)=>{
+        console.log(i, val);
+        // array.push(val);
+        array[i] = val;
+        count++;
+        if (count === tasks.length) {
+          callback(array);
+        }
+      });
+    })(i);
+  }
+};
+
+
+// solve using promise
+
+// first, create a myPromisify func to turn an async function to a promise
+// solve using promise
+
+// create a myPromisify func to turn an async function to a promise
+var myPromisify = (asyncFunc) =>{
+  return new Promise((resolve, reject) => {
+    asyncFunc((val)=>{
+      resolve(val);
+    });
+  });
+};
+//OR
+var myPromisify = (asyncFunc) =>{
+  return new Promise((resolve, reject) => {
+    asyncFunc(resolve); //resolve is a function takes in a value
+  });
+};
+
+// Use promise.all
+var asyncMap = (tasks, callback) =>{
+  var promiseArr = [];
+  promiseArr = tasks.map(task=>{
+    return myPromisify(task);
+  });
+  
+  Promise.all(promiseArr)
+  .then((val)=>{
+    callback(val);
+  });
+};
+
+// OR, shortened promise version
+var asyncMap = (tasks, callback) =>{
+  var promiseArr = [];
+  promiseArr = tasks.map(myPromisify);
+  
+  Promise.all(promiseArr)
+  .then(callback);
+};
+
+
+
 module.exports = { asyncMap };
-asyncMap([
- cb => {
-   setTimeout(() => {
-     cb('one');
-   }, 200);
- },
- cb => {
-   setTimeout(() => {
-     cb('two');
-   }, 100);
- }
-],
- results => {
-   // the results array will equal ['one','two'] even though
-   // the second function had a shorter timeout.
-   console.log(results); // ['one', 'two']
-});
+
+
+ asyncMap([
+  cb => {
+    setTimeout(() => {
+      cb('one');
+    }, 200);
+  },
+  cb => {
+    setTimeout(() => {
+      cb('two');
+    }, 100);
+  }
+ ],
+  results => {
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+ });

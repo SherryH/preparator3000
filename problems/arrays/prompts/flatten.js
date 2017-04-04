5c1aaa4e369628e51a2aed5036771dacec3401d0
SherryH <enjoy.wind7rain@gmail.com>
2017-04-05 07:24:48 +1200

    Added flattened.js

--- problems/arrays/prompts/flatten.js
+++ problems/arrays/prompts/flatten.js
@@ -1,36 +1,55 @@
 'use strict';
 
 // flatten a nested array into a flat (1D) array
-const flatten = arrays => {
-	// your code here
-  var results = [];
-  for (var i = 0; i < arrays.length; i++) {
-    if (checkArray(arrays[i])) {
-      flattenArray(arrays[i])
+// const flatten = arrays => {
+// 	// your code here
+//   var results = [];
+//   for (var i = 0; i < arrays.length; i++) {
+//     if (checkArray(arrays[i])) {
+//       flattenArray(arrays[i])
+//     } else {
+//       results.push(arrays[i])
+//     }
+//   }
+
+//   function checkArray(array) {
+//     if (Array.isArray(array)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   function flattenArray(array) {
+//     for (var i = 0; i < array.length; i++) {
+//       if (checkArray(array[i])) {
+//         flattenArray(array[i])
+//       } else {
+//         results.push(array[i])
+//       }
+//     }
+//   }
+
+//   return results;
+// };
+
+const flatten = (arrays, flattenedArr=[]) => {
+  // for each elem in the arrays
+  // check if it is an array
+  // if array
+  //    continue flatten recursion
+  // else
+  //    add to the final array
+  arrays.forEach((item) => {
+    if(Array.isArray(item)) {
+      flatten(item, flattenedArr);
     } else {
-      results.push(arrays[i])
+      flattenedArr.push(item);
     }
-  }
+  });
 
-  function checkArray(array) {
-    if (Array.isArray(array)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  return flattenedArr;
+}
 
-  function flattenArray(array) {
-    for (var i = 0; i < array.length; i++) {
-      if (checkArray(array[i])) {
-        flattenArray(array[i])
-      } else {
-        results.push(array[i])
-      }
-    }
-  }
-
-  return results;
-};
 
 module.exports = { flatten };

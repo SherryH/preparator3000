68d74ef3fbd6962c4e962ab3de5cbbb510b5903c
SherryH <enjoy.wind7rain@gmail.com>
2017-04-13 07:27:16 +1200

    Added reverse-array

--- problems/arrays/prompts/reverse_array_in_place.js
+++ problems/arrays/prompts/reverse_array_in_place.js
@@ -3,7 +3,11 @@
 // reverse and return an array in place (space complexity of O(1))
 const reverseArrayInPlace = array => {
   // your code here
-  return array.reverse();
+  var newArr = [];
+  while (array.length) {
+  	newArr.push(array.pop());
+  }
+  return newArr;
 };
 
 module.exports = { reverseArrayInPlace };

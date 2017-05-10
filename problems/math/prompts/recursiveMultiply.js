aa0bb1a29c02c9f279332c4e71511fca76fb5a10
SherryH <enjoy.wind7rain@gmail.com>
2017-05-10 21:35:28 +1200

    Implemented multiply with recursion

--- problems/math/prompts/recursiveMultiply.js
+++ problems/math/prompts/recursiveMultiply.js
@@ -12,6 +12,15 @@
 
 const recursiveMultiply = (num1, num2) => {
   // TODO: Implement recursive multiply
+  if (num2 ===1) {
+    return num1;
+  } else if (num2 === 0) {
+    return 0;
+  } else if (num1 > 0 && num2 > 0){
+    return recursiveMultiply(num1, num2-1) + num1;
+  } else if (num1< 0 && num2 <0) {
+    return recursiveMultiply(num1, num2+1) - num1;
+  }
 };
 
 module.exports = { recursiveMultiply };

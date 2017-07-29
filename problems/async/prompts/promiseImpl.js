146e651a67720b465040325c44b32a247c8dc564
SherryH <enjoy.wind7rain@gmail.com>
2017-07-29 21:00:10 +1200

    Implemented the simpliest usecase of promise

--- problems/async/prompts/promiseImpl.js
+++ problems/async/prompts/promiseImpl.js
@@ -9,3 +9,43 @@ asyncFunc((val) => {
     console.log('hi', val);
 });
 
+//promisify the async function
+var myPromisify = (callback) => {
+    //Promise takes in a func, which takes 2 inputs: resolve and reject
+    // resolve will get the value once async function is resolved
+    // and that value will be available in .then()
+    return new Promise((resolve, reject) => {
+        callback(resolve);
+    });
+};
+
+var proAsyncFunc = myPromisify(asyncFunc);
+
+proAsyncFunc
+    .then((val) => {
+        console.log('from proAsyncFunc', val);
+    });
+
+ // Start with examples in http://www.mattgreer.org/articles/promises-in-wicked-detail/
+ function doSomthing(callback){
+    value = 'the value 42';
+    callback(value);
+ };
+
+ doSomthing(function(val){
+    console.log('Value passed into callback:',val);
+ });
+
+ //if we want to achieve the following:
+ doSomething2().then(function(val){
+    console.log('Value passed via promise',val);
+ });
+ //then we need to define doSomething2 as.....
+ function doSomething2(){
+    return {
+        then: function(callback){
+            value = 'the promise value 55'
+            callback(value);
+        }
+    };
+ };
